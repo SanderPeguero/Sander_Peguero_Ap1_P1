dc4f80de1f18dcaad2da2f74b55f356eac151b40
Sander Peguero <sanderpeguero.21@gmail.com>
2022-02-02 02:28:35 -0400

    Working in UI/Consultas

---
+++ UI/Consultas/Consulta.cs
@@ -0,0 +1,42 @@
+using System.Windows;
+using Sander_Peguero_Ap1_P1.Entidades;
+using Sander_Peguero_Ap1_P1.BLL;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Clase2.UI.Consultas
+{
+    public partial class cLibros : Window
+    {
+
+        public cLibros()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Entidades>();
+
+            if (string.IsNullOrWhiteSpace(CriterioTextBox.Text))//si no hay criterio, busco todos         
+                listado = ;
+            else
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0: //"Titulo"
+                        listado = ;
+                        break;
+                    case 1:   //"Grupo" 
+                        listado = ;
+                        break;
+                }
+            }
+
+            DataGrid.ItemsSource = null;
+            DataGrid.ItemsSource = listado;
+
+        }
+
+    }
+}
