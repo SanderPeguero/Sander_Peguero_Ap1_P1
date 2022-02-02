55eefbf45c0724e395034423680876cf0fe7bc91
Sander Peguero <95009440+SanderPeguero@users.noreply.github.com>
2022-02-01 23:11:02 -0400

    Created a New WPF Project

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sander_Peguero_Ap1_P1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
