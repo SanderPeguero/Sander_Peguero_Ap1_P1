ebe45db1f93023a59a82074ed8bd5e42c5dc086a
Sander Peguero <sanderpeguero.21@gmail.com>
2022-02-07 18:30:43 -0400

    Creating the "Producto" entity

--- Entidades/Entidad.cs
+++ Entidades/Entidad.cs
@@ -1,7 +1,17 @@
 
-namespace Sander_Peguero_Ap1_P1.Entidades{
-    public class Entidad{
+namespace Sander_Peguero_Ap1_P1.Producto{
 
+    public class Producto{
+        public int ProductoId { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int Existencia { get; set; }
+
+        public int Costo { get; set; }
+
+        public int ValorInventario { get; set; }
+        
     }
     
 }
