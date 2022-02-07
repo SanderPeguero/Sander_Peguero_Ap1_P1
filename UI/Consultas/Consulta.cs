c85d9276d48a143926facdd93bc06b2441ea547d
Sander Peguero <sanderpeguero.21@gmail.com>
2022-02-07 19:04:08 -0400

    Error Fix to "Consulta"

--- UI/Consultas/Consulta.cs
+++ UI/Consultas/Consulta.cs
@@ -1,5 +1,5 @@
 using System.Windows;
-using Sander_Peguero_Ap1_P1.Entidades;
+using Sander_Peguero_Ap1_P1.Producto;
 using Sander_Peguero_Ap1_P1.BLL;
 using System.Linq;
 using System.Collections.Generic;
@@ -16,22 +16,22 @@ namespace Clase2.UI.Consultas
 
         private void BuscarButton_Click(object sender, RoutedEventArgs e)
         {
-            var listado = new List<Entidades>();
+            var listado = new List<Producto>();
 
-            if (string.IsNullOrWhiteSpace(CriterioTextBox.Text))//si no hay criterio, busco todos         
-                listado = ;
-            else
-            {
-                switch (FiltroComboBox.SelectedIndex)
-                {
-                    case 0: //"Titulo"
-                        listado = ;
-                        break;
-                    case 1:   //"Grupo" 
-                        listado = ;
-                        break;
-                }
-            }
+            // if (string.IsNullOrWhiteSpace(CriterioTextBox.Text))//si no hay criterio, busco todos         
+            //     listado = ;
+            // else
+            // {
+            //     switch (FiltroComboBox.SelectedIndex)
+            //     {
+            //         case 0: //"Titulo"
+            //             listado = ;
+            //             break;
+            //         case 1:   //"Grupo" 
+            //             listado = ;
+            //             break;
+            //     }
+            // }
 
             DataGrid.ItemsSource = null;
             DataGrid.ItemsSource = listado;
