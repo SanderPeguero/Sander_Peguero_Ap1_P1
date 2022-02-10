4761b7e4023ff6f11e5250d790c77edf3677aa1a
Sander Peguero <sanderpeguero.21@gmail.com>
2022-02-10 01:30:20 -0400

    Validations, Methods and User Experience in "Registros"
    -Setted the initial value of the Producto Id in the UI to be 1.
    -Added an enter event to the Producto Id in the UI to find when pressed.
    -Developed the edit method
    -Fixed validations exceptions

--- UI/Registros/Registro.xaml.cs
+++ UI/Registros/Registro.xaml.cs
@@ -15,6 +15,7 @@ namespace Sander_Peguero_Ap1_P1.UI.Registros
 
             InitializeComponent();
 
+            producto.ProductoId = 1;
             this.DataContext = producto;
 
         }
@@ -35,20 +36,88 @@ namespace Sander_Peguero_Ap1_P1.UI.Registros
 
         }
 
+        private void Buscar(Producto product)
+        {
+
+            var encontrado = BLL.BLL.Buscar(product.ProductoId);
+
+            if (encontrado != null)
+            {
+
+                this.producto = encontrado;
+                Cargar();
+
+            }
+            else
+            {
+
+                Limpiar();
+                MessageBox.Show("No Se Encontro en la Base de Datos", "Base de Datos", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+
+        }
+
+        private void ProductoIdEnterEvent(object sender, KeyEventArgs e)
+        {
+
+            if (e.Key == Key.Return)
+            {
+
+                producto.ProductoId = int.Parse(ProductoIdTextBox.Text);
+                Buscar(this.producto);
+
+            }
+            else
+            {
+
+                return;
+
+            }
+
+        }
+
         private void SumaAutomatica(object sender, KeyEventArgs e)
         {
 
-            if (e.Key == Key.Return && Validar())
+            if (e.Key == Key.Return)
             {
+                
+                producto.ProductoId = int.Parse(ProductoIdTextBox.Text);
+                producto.Descripcion = DescripcionTextBox.Text;
+                producto.Costo = int.Parse(CostoTextBox.Text);
+                producto.Existencia = int.Parse(ExistenciaTextBox.Text);
+                producto.ValorInventario = int.Parse(ValorInventarioTextBox.Text);
+
+                if (producto.Costo < 0)
+                {
+
+                    CostoTextBox.Focus();
+                    MessageBox.Show("El Costo del Producto no Puede Ser Menor que 1", "Error en Costo", MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+
+                }
+
+                if (producto.Existencia < 0)
+                {
+
+                    ExistenciaTextBox.Focus();
+                    MessageBox.Show("La Cantidad en Existencia no Puede Ser Menor que 1", "Error en Existencia", MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+
+                }
 
                 int Costo = int.Parse(CostoTextBox.Text);
                 int Existencia = int.Parse(ExistenciaTextBox.Text);
 
                 ValorInventarioTextBox.Text = (Costo * Existencia).ToString();
 
-            }else{
+            }
+            else
+            {
 
                 return;
+
             }
 
         }
@@ -57,6 +126,12 @@ namespace Sander_Peguero_Ap1_P1.UI.Registros
         {
 
             bool esValido = true;
+
+            producto.ProductoId = int.Parse(ProductoIdTextBox.Text);
+            producto.Descripcion = DescripcionTextBox.Text;
+            producto.Costo = int.Parse(CostoTextBox.Text);
+            producto.Existencia = int.Parse(ExistenciaTextBox.Text);
+            producto.ValorInventario = int.Parse(ValorInventarioTextBox.Text);
 
             if (producto.ProductoId < -1)
             {
@@ -90,7 +165,7 @@ namespace Sander_Peguero_Ap1_P1.UI.Registros
                 MessageBox.Show("No Pueden Existir 2 Descripciones Iguales en la Base de Datos", "Error en Descripcion", MessageBoxButton.OK, MessageBoxImage.Information);
 
             }
-            else if (producto.Costo < -1)
+            else if (producto.Costo < 0)
             {
 
                 esValido = false;
@@ -98,7 +173,7 @@ namespace Sander_Peguero_Ap1_P1.UI.Registros
                 MessageBox.Show("El Costo del Producto no Puede Ser Menor que 1", "Error en Costo", MessageBoxButton.OK, MessageBoxImage.Information);
 
             }
-            else if (producto.Existencia < -1)
+            else if (producto.Existencia < 0)
             {
 
                 esValido = false;
@@ -127,7 +202,7 @@ namespace Sander_Peguero_Ap1_P1.UI.Registros
             {
 
                 Limpiar();
-                MessageBox.Show("No Se Encontro en la Base de Datos", "Base de Datos", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("No Se Encontro en la Base de Datos", "Base de Datos", MessageBoxButton.OK, MessageBoxImage.Information);
 
             }
 
@@ -143,7 +218,47 @@ namespace Sander_Peguero_Ap1_P1.UI.Registros
         private void GuardarButton_Click(object sender, RoutedEventArgs e)
         {
 
-            bool paso = false;
+            if (!Validar())
+            {
+
+                return;
+
+            }
+            else
+            {
+
+                producto.ProductoId = int.Parse(ProductoIdTextBox.Text);
+                producto.Descripcion = DescripcionTextBox.Text;
+                producto.Costo = int.Parse(CostoTextBox.Text);
+                producto.Existencia = int.Parse(ExistenciaTextBox.Text);
+                producto.ValorInventario = int.Parse(ValorInventarioTextBox.Text);
+
+                if (BLL.BLL.Guardar(producto))
+                {
+
+                    MessageBox.Show("Guardado con éxito", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                }
+                else
+                {
+
+                    MessageBox.Show("No se ha Podido Guardar", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                }
+
+            }
+        }
+
+        private void EditarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            BLL.BLL.Eliminar(producto.ProductoId);
+
+            producto.ProductoId = int.Parse(ProductoIdTextBox.Text);
+            producto.Descripcion = DescripcionTextBox.Text;
+            producto.Costo = int.Parse(CostoTextBox.Text);
+            producto.Existencia = int.Parse(ExistenciaTextBox.Text);
+            producto.ValorInventario = int.Parse(ValorInventarioTextBox.Text);
 
             if (!Validar())
             {
@@ -152,38 +267,16 @@ namespace Sander_Peguero_Ap1_P1.UI.Registros
 
             }
 
-            producto.ProductoId = int.Parse(ProductoIdTextBox.Text);
-            producto.Descripcion = DescripcionTextBox.Text;
-            producto.Costo = int.Parse(CostoTextBox.Text);
-            producto.Existencia = int.Parse(ExistenciaTextBox.Text);
-            producto.ValorInventario = int.Parse(ValorInventarioTextBox.Text);
-
-            paso = BLL.BLL.Guardar(producto);
-
-            if (paso)
+            if (BLL.BLL.Guardar(producto))
             {
 
-                MessageBox.Show("Guardado con éxito", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show("Editado con Exito", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
 
             }
             else
             {
 
-                MessageBox.Show("No se ha Podido Guardar", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-
-            }
-
-        }
-
-        private void EditarButton_Click(object sender, RoutedEventArgs e)
-        {
-
-            bool paso = false;
-
-            if (paso)
-            {
-
-                MessageBox.Show("Editado con Exito", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show("No se ha Podido Editar", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
 
             }
 
@@ -191,7 +284,7 @@ namespace Sander_Peguero_Ap1_P1.UI.Registros
 
         private void EliminarButton_Click(object sender, RoutedEventArgs e)
         {
-            
+
             producto.ProductoId = int.Parse(ProductoIdTextBox.Text);
 
             if (BLL.BLL.Eliminar(producto.ProductoId))
